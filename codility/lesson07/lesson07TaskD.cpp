f1c0f8c6ee3e09ba6065dc60f5cb96fe1f5554d5
Philipp Hacker <pha@pha-MBP.fritz.box>
2024-06-25 12:04:36 +0100

    lesson07TaskD3

--- codility/lesson07/lesson07TaskD.cpp
+++ codility/lesson07/lesson07TaskD.cpp
@@ -11,11 +11,36 @@ using namespace helper;
 
 int solution(std::vector<int>& H)
 {
-  int blocks = 1;
+  int blocks = 0;
 
-  int height = -1;
-  for (size_t i = 0; i < H.size(); i++) {}
+  std::vector<int> heights;
+  std::vector<int> widths;
+  std::vector<int> stack;
+  stack.push_back(H[0]);
 
+  int width = 0;
+  for (size_t i = 0; i < H.size(); i++) {
+    if (H[i] < stack.back()) {
+      blocks++;
+
+      heights.push_back(stack.back());
+      widths.push_back(width);
+      width = 0;
+
+      stack.pop_back();
+      stack.push_back(H[i]);
+    }
+    else if (H[i] > stack.back()) {
+      stack.push_back(H[i]);
+    }
+    width++;
+
+    print<char[], int>("stack", stack);
+    print<char[], int>("blocks", blocks);
+  }
+
+  print<char[], int>("heights", heights);
+  print<char[], int>("widths", widths);
 
   return blocks;
 } // solution
@@ -29,6 +54,9 @@ int main()
   int result = -1;
 
   std::vector<int> vector = gen.randomVector(N);
+  vector = {8, 8, 5, 7, 9, 8, 7, 4, 8}; // 7
+  // vector = {8, 8, 5, 7, 9, 9, 7, 4, 8};    // 6
+  // vector = {8, 8, 5, 7, 9, 8, 7, 4, 8, 4}; // 7
 
   Timer t;
   t.tick();
